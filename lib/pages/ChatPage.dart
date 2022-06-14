693ffb2494017e114b2cd4d0205f9a9bdb034a6c
Manav Sarkar <manavsarkar07@gmail.com>
2022-06-14 16:04:40 +0530

    Chat Message refresh fix

--- lib/pages/ChatPage.dart
+++ lib/pages/ChatPage.dart
@@ -21,16 +21,18 @@ class ChatPage extends StatefulWidget {
 
 class ChatPageState extends State<ChatPage> {
   List<Msg> messageList = [];
-  void refresh() {
-    setState(() {
-      ;
-    });
+  void refreshMessages() {
+    if (Global.conversations[widget.converser] != null) {
+      Global.conversations[widget.converser]!.forEach((key, value) {
+        messageList.add(value);
+      });
+    }
   }
 
+  @override
   void initState() {
-    Global.conversations[widget.converser]!.forEach((key, value) {
-      messageList.add(value);
-    });
+    super.initState();
+    refreshMessages();
   }
 
   ScrollController _scrollController = new ScrollController();
@@ -38,113 +40,113 @@ class ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     TextEditingController myController = TextEditingController();
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: Text('Chat with ' + widget.converser),
-        ),
-        body: SingleChildScrollView(
-            reverse: true,
-            child: Padding(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom),
-              child: Column(
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * .8,
-                    child: messageList == null
-                        ? Text("no messages")
-                        : ListView.builder(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            // reverse: true,
-                            controller: _scrollController,
-                            padding: const EdgeInsets.all(8),
-                            itemCount:
-                                messageList == null ? 0 : messageList.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return Container(
-                                height: 55,
-                                child: messageList[index].msgtype == 'sent'
-                                    ? Bubble(
-                                        margin: BubbleEdges.only(top: 10),
-                                        nip: BubbleNip.rightTop,
-                                        color: Color(0xffd1c4e9),
-                                        child: Text(
-                                            messageList[index].msgtype +
-                                                ": " +
-                                                messageList[index].message,
-                                            textAlign: TextAlign.right),
-                                      )
-                                    : Bubble(
-                                        nip: BubbleNip.leftTop,
-                                        color: Color(0xff80DEEA),
-                                        margin: BubbleEdges.only(top: 10),
-                                        child: Text(
-                                          messageList[index].msgtype +
-                                              ": " +
-                                              messageList[index].message,
-                                        ),
-                                      ),
-                              );
-                            },
-                          ),
-                  ),
-                  TextFormField(
-                    controller: myController,
-                    decoration: const InputDecoration(
-                      icon: Icon(Icons.person),
-                      hintText: 'Send Message?',
-                      labelText: 'Send Message ',
-                    ),
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        var msgId = nanoid(21);
-                        var data = {
-                          "sender": "$Global.myName",
-                          "receiver": "$widget.device.deviceName",
-                          "message": "$myController.text",
-                          "id": "$msgId",
-                          "Timestamp": "${DateTime.now().toUtc().toString()}",
-                          "type": "Payload"
-                        };
-                        var Mesagedata = data.toString();
-                        Global.cache[msgId] = Payload(
-                            msgId,
-                            Global.myName,
-                            widget.converser,
-                            myController.text,
-                            DateTime.now().toUtc().toString());
-                        insertIntoMessageTable(Payload(
-                            msgId,
-                            Global.myName,
-                            widget.converser,
-                            myController.text,
-                            DateTime.now().toUtc().toString()));
-                        // Global.devices.forEach((element) {
-                        //   Global.nearbyService!
-                        //       .sendMessage(element.deviceId, Mesagedata);
-                        // });
-                        // Global.nearbyService!
-                        //     .sendMessage(widget.device.deviceId, myController.text);
-                        setState(() {
-                          // Global
-                          //     .conversations[widget.device.deviceName][msgId](new Msg(widget.device.deviceId,
-                          //         myController.text, "sent"));
-                          Global.conversations[widget.converser]![msgId] = Msg(
-                              myController.text,
-                              "sent",
-                              data["Timestamp"]!,
-                              msgId);
-                          insertIntoConversationsTable(
-                              Msg(myController.text, "sent", data["Timestamp"]!,
-                                  msgId),
-                              widget.converser);
-                        });
-                      },
-                      child: Text("send")),
-                ],
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text('Chat with ' + widget.converser),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Padding(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * .8,
+                child: messageList == null
+                    ? Text("no messages")
+                    : ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        // reverse: true,
+                        controller: _scrollController,
+                        padding: const EdgeInsets.all(8),
+                        itemCount: messageList == null ? 0 : messageList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            height: 55,
+                            child: messageList[index].msgtype == 'sent'
+                                ? Bubble(
+                                    margin: BubbleEdges.only(top: 10),
+                                    nip: BubbleNip.rightTop,
+                                    color: Color(0xffd1c4e9),
+                                    child: Text(
+                                        messageList[index].msgtype +
+                                            ": " +
+                                            messageList[index].message,
+                                        textAlign: TextAlign.right),
+                                  )
+                                : Bubble(
+                                    nip: BubbleNip.leftTop,
+                                    color: Color(0xff80DEEA),
+                                    margin: BubbleEdges.only(top: 10),
+                                    child: Text(
+                                      messageList[index].msgtype +
+                                          ": " +
+                                          messageList[index].message,
+                                    ),
+                                  ),
+                          );
+                        },
+                      ),
               ),
-            )));
+              TextFormField(
+                controller: myController,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'Send Message?',
+                  labelText: 'Send Message ',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  var msgId = nanoid(21);
+                  var data = {
+                    "sender": "$Global.myName",
+                    "receiver": "$widget.device.deviceName",
+                    "message": "$myController.text",
+                    "id": "$msgId",
+                    "Timestamp": "${DateTime.now().toUtc().toString()}",
+                    "type": "Payload"
+                  };
+                  var Mesagedata = data.toString();
+                  Global.cache[msgId] = Payload(
+                      msgId,
+                      Global.myName,
+                      widget.converser,
+                      myController.text,
+                      DateTime.now().toUtc().toString());
+                  insertIntoMessageTable(Payload(
+                      msgId,
+                      Global.myName,
+                      widget.converser,
+                      myController.text,
+                      DateTime.now().toUtc().toString()));
+                  // Global.devices.forEach((element) {
+                  //   Global.nearbyService!
+                  //       .sendMessage(element.deviceId, Mesagedata);
+                  // });
+                  // Global.nearbyService!
+                  //     .sendMessage(widget.device.deviceId, myController.text);
+                  setState(() {
+                    // Global
+                    //     .conversations[widget.device.deviceName][msgId](new Msg(widget.device.deviceId,
+                    //         myController.text, "sent"));
+                    Global.conversations[widget.converser]![msgId] = Msg(
+                        myController.text, "sent", data["Timestamp"]!, msgId);
+                    insertIntoConversationsTable(
+                        Msg(myController.text, "sent", data["Timestamp"]!,
+                            msgId),
+                        widget.converser);
+                  });
+                  refreshMessages();
+                },
+                child: Text("send"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
