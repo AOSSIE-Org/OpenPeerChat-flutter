381df4f633a6f34cf5b187a4b076a8be49c306c5
Manav Sarkar <manavsarkar07@gmail.com>
2022-07-04 16:34:03 +0530

    Implemented Save profile feature
    Now the user will set the username for first time after installing and then it will navigate directly to devices list screen.

--- lib/pages/Profile.dart
+++ lib/pages/Profile.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_nearby_connections_example/pages/DeviceListScreen.dart';
 import 'package:nanoid/nanoid.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../classes/Global.dart';
 import 'DeviceListScreen.dart';
 
@@ -12,52 +16,92 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   // TextEditingController for the name of the user
   TextEditingController myName = TextEditingController();
-
+  bool loading = true;
   // Custom generated id for the user
   var customLengthId = nanoid(6);
+
+  // Fetching details from saved profile
+  // If no profile is saved, then the new values are used
+  // else navigate to DeviceListScreen
+  Future getDetails() async {
+    // Obtain shared preferences.
+    final prefs = await SharedPreferences.getInstance();
+    final name = prefs.getString('p_name') ?? '';
+    final id = prefs.getString('p_id') ?? '';
+    setState(() {
+      myName.text = name;
+    });
+    if (name.isNotEmpty && id.isNotEmpty) {
+      navigateToDeviceListScreen();
+    } else {
+      setState(() {
+        loading = false;
+      });
+    }
+  }
+
+  void navigateToDeviceListScreen() {
+    Global.myName = myName.text;
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DevicesListScreen(
+          deviceType: DeviceType.browser,
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getDetails();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
+      appBar: AppBar(
+        title: Text(
+          'Profile',
+        ),
+      ),
+      body: Visibility(
+        visible: loading,
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+        replacement: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            SizedBox(
-              height: 100,
-            ),
             Text("Your Username will be your name+$customLengthId"),
-            TextFormField(
-              controller: myName,
-              decoration: const InputDecoration(
-                icon: Icon(Icons.person),
-                hintText: 'What do people call you?',
-                labelText: 'Name *',
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: myName,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'What do people call you?',
+                  labelText: 'Name *',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (String? value) {
+                  return (value != null &&
+                          value.contains('@') &&
+                          value.length > 3)
+                      ? 'Do not use the @ char and name length should be greater than 3'
+                      : null;
+                },
               ),
-              onSaved: (String? value) {
-                // This optional block of code can be used to run
-                // code when the user saves the form.
-              },
-              validator: (String? value) {
-                return (value != null && value.contains('@'))
-                    ? 'Do not use the @ char.'
-                    : null;
-              },
-            ),
-            SizedBox(
-              height: 20,
             ),
             ElevatedButton(
-              onPressed: () {
-                // Global.myName = myName.text+custom_length_id;
-                Global.myName = myName.text;
-
+              onPressed: () async {
+                final prefs = await SharedPreferences.getInstance();
+                // saving the name and id to shared preferences
+                prefs.setString('p_name', myName.text);
+                prefs.setString('p_id', customLengthId);
                 // On pressing, move to the device list screen
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) =>
-                        DevicesListScreen(deviceType: DeviceType.browser),
-                  ),
-                );
+                navigateToDeviceListScreen();
               },
               child: Text("Save"),
             )
