b1a8880c21cbc062363c13b9b1728c11f6cce124
Joymakwana2003 <joy.makwana@iitgn.ac.in>
2023-03-20 19:12:35 +0530

    Successfully migrated to V2

--- lib/encyption/rsa.dart
+++ lib/encyption/rsa.dart
@@ -2,10 +2,10 @@ import 'package:rsa_encrypt/rsa_encrypt.dart';
 import 'package:pointycastle/api.dart' as crypto;
 
 //Future to hold our KeyPair
-Future<crypto.AsymmetricKeyPair> futureKeyPair;
+late Future<crypto.AsymmetricKeyPair> futureKeyPair;
 
 //to store the KeyPair once we get data from our future
-crypto.AsymmetricKeyPair keyPair;
+late crypto.AsymmetricKeyPair keyPair;
 
 Future<crypto.AsymmetricKeyPair<crypto.PublicKey, crypto.PrivateKey>> getKeyPair()
 {
@@ -16,6 +16,5 @@ Future<crypto.AsymmetricKeyPair<crypto.PublicKey, crypto.PrivateKey>> getKeyPair
 void createPair (){
   futureKeyPair=getKeyPair();
   futureKeyPair.then((value) => keyPair=value);
-  var x=encodePrivateKeyToPemPKCS1(keyPair.privateKey);
 
 }
